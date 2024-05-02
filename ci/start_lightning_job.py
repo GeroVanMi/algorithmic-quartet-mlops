1e594a84a67df9763a8ea13e8fb90471cff1219b
Gerome Meyer <gerome.meyer@protonmail.com>
2024-05-02 09:49:58 +0200

    Restored full pipeline in cloudbuild.yml

--- ci/start_lightning_job.py
+++ ci/start_lightning_job.py
@@ -1,5 +1,3 @@
-import os
-
 from lightning_sdk import Machine
 from studio_helper import create_studio
 
@@ -14,26 +12,20 @@ if __name__ == "__main__":
         print("Studio is already running. Not starting it again.")
 
     BASE_PATH = "/teamspace/studios/this_studio/algorithmic-quartet-mlops"
-    # Update the code to the latest commit
-    # studio.run(f"git -C {BASE_PATH} reset --hard origin/main")
-    #
-    # # Update the dependencies
-    # studio.run(f"pip install -r {BASE_PATH}/training/requirements.txt")
-    #
-    # # Setup the cloud storage bucket
-    # studio.run(f"bash {BASE_PATH}/training/setup_gc.sh")
 
-    # Run the pipeline on the CPU once, to make sure that there are no errors
-    # output, exit_code = studio.run(f"python {BASE_PATH}/training/pipeline.py")
-
-    # If there are errors, raise an exception and stop the execution!
-    # if exit_code != 0:
-    #     print(output)
-    #     raise RuntimeError("Training pipeline did not run successfully!")
-    #
     # Use the jobs plugin
     jobs_plugin = studio.installed_plugins["jobs"]
 
     # Start the training pipeline on a GPU job
-    cmd = f"python {BASE_PATH}/training/pipeline.py"
-    jobs_plugin.run(cmd, name="Train model", machine=Machine.CPU)  # type: ignore
+    # cmd = f"""cat ~/keys/ar-read-only.json | docker login -u _json_key_base64 --password-stdin https://europe-west9-docker.pkg.dev && \
+    # docker run --gpus all europe-west9-docker.pkg.dev/algorithmic-quartet/training-pipelines/pokemon-trainer:latest
+    # """
+    studio.run(
+        "cat ~/keys/ar-read-only.json | docker login -u _json_key_base64 --password-stdin https://europe-west9-docker.pkg.dev"
+    )
+    studio.run(
+        "docker run --gpus all europe-west9-docker.pkg.dev/algorithmic-quartet/training-pipelines/pokemon-trainer:latest"
+    )
+    # jobs_plugin.run(cmd, name="Train model", machine=Machine.CPU)  # type: ignore
+
+    studio.stop()
