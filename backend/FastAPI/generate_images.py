6d7d6a399fb30e77606253bd30f79b2023e3e06c
Gerome Meyer <gerome.meyer@protonmail.com>
2024-05-06 15:46:40 +0200

    Added image generation code

--- backend/FastAPI/generate_images.py
+++ backend/FastAPI/generate_images.py
@@ -2,14 +2,16 @@ import os
 from pathlib import Path
 
 import torch
+import wandb
 from accelerate import Accelerator
 from Configuration import Configuration
 from diffusers.models.unets.unet_2d import UNet2DModel
 from diffusers.pipelines.ddpm.pipeline_ddpm import DDPMPipeline
 from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
-from diffusers.utils.pil_utils import make_image_grid
+from PIL.Image import Image
 
 
+# TODO: This code should be imported from `training/src/model/create_model`
 def create_model(config: Configuration):
     return UNet2DModel(
         sample_size=config.image_size,  # the target image resolution
@@ -43,7 +45,35 @@ def create_model(config: Configuration):
     )
 
 
-def prediction():
+def save_images(images: list[Image]):
+    # TODO: Store images in the cloud bucket
+    predictions_directory = Path("predictions/")
+    os.makedirs(predictions_directory, exist_ok=True)
+
+    for index, image in enumerate(images):
+        image.save(f"{predictions_directory}/{index:0>2}.png")
+
+
+def initialize_pipeline(accelerator, config) -> DDPMPipeline:
+    run = wandb.init()
+    if run is None:
+        raise RuntimeError("Couldn't initalize W&B!")
+
+    artifact = run.use_artifact(
+        "algorithmic-quartet-zhaw/model-registry/pokemon-generator:latest", type="model"
+    )
+    model_path = Path("model/pokemon-generator")
+    artifact.download(root=model_path)
+
+    noise_scheduler = DDPMScheduler(num_train_timesteps=config.number_of_noise_steps)
+    model = create_model(config)
+    pipeline = DDPMPipeline(
+        unet=accelerator.unwrap_model(model), scheduler=noise_scheduler
+    )
+    return pipeline.from_pretrained(model_path)  # type: ignore (There are multiple definitions which break the type hints)
+
+
+def generate_images():
     config = Configuration()
     accelerator = Accelerator(
         mixed_precision=config.mixed_precision,
@@ -52,30 +82,17 @@ def prediction():
         project_dir=os.path.join(config.output_dir, "logs"),
     )
 
-    noise_scheduler = DDPMScheduler(num_train_timesteps=config.number_of_noise_steps)
-    model = create_model(config)
-    pipeline = DDPMPipeline(
-        unet=accelerator.unwrap_model(model), scheduler=noise_scheduler
-    )
-    # TODO: Download pipeline files from W&B model registry
-    pipeline.from_pretrained("../training/models/pokemon-generator/")
+    image_generation_pipeline = initialize_pipeline(accelerator, config)
 
-    images = pipeline(
+    images = image_generation_pipeline(
         batch_size=config.eval_batch_size,
         generator=torch.manual_seed(config.seed),
-        num_inference_steps=config.number_of_noise_steps,  # TODO: This needs to be set via the config and synced with the training equivalent
-    ).images  # type: ignore
+        num_inference_steps=config.number_of_noise_steps,
+    ).images  # type: ignore (There are multiple definitions which break the type hints)
 
-    # Make a grid out of the images
-    image_grid = make_image_grid(images, rows=4, cols=4)  # type: ignore
-
-    # Save the images
-    predictions_directory = Path("predictions/")
-    os.makedirs(predictions_directory, exist_ok=True)
-
-    # TODO: Store images in the cloud bucket
-    image_grid.save(f"{predictions_directory}/new_images.png")
+    if isinstance(images, list):
+        save_images(images)
 
 
 if __name__ == "__main__":
-    prediction()
+    generate_images()
