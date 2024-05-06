1d22f8ae899c1e0705aaaa96012c19cd0c2ac99d
Gerome Meyer <gerome.meyer@protonmail.com>
2024-05-06 18:53:29 +0200

    Added more images and more complex model

--- training/src/configurations/TrainConfig.py
+++ training/src/configurations/TrainConfig.py
@@ -5,7 +5,7 @@ from .Configuration import Configuration
 
 @dataclass
 class TrainConfig(Configuration):
-    train_batch_size = 16
+    train_batch_size = 24
     eval_batch_size = 8
     num_epochs = 10
-    num_images = 100
+    num_images = 1000
--- training/src/model/create_model.py
+++ training/src/model/create_model.py
@@ -10,22 +10,25 @@ def create_model(config: Configuration):
         layers_per_block=2,  # how many ResNet layers to use per UNet block
         block_out_channels=(
             128,
+            128,
             256,
+            256,
+            512,
             512,
         ),  # the number of output channels for each UNet block
         down_block_types=(
             "DownBlock2D",  # a regular ResNet downsampling block
             "DownBlock2D",
-            # "DownBlock2D",
-            # "DownBlock2D",
-            # "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+            "DownBlock2D",
+            "DownBlock2D",
+            "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
             "DownBlock2D",
         ),
         up_block_types=(
             "UpBlock2D",  # a regular ResNet upsampling block
-            # "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-            # "UpBlock2D",
-            # "UpBlock2D",
+            "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+            "UpBlock2D",
+            "UpBlock2D",
             "UpBlock2D",
             "UpBlock2D",
         ),
