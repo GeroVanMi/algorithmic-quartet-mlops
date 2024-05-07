92510285df5e15e99e69d1941b9e90165041d7d7
Yves Bähler <baehlery@gmail.com>
2024-05-07 14:58:55 +0200

    Added the PKM prediction instead of Iris. Removed all Iris related things. Removed the MLServer - failed experiment. The images arent represented serverside - but should be able to be pulled for the Streamlit app

--- backend/FastAPI/app.py
+++ backend/FastAPI/app.py
@@ -1,14 +1,17 @@
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI
-from model import IrisModel, IrisSpecies
+#from model import IrisModel, IrisSpecies
+from generate_images import *
 
 # 2. Create app and model objects
 app = FastAPI()
-model = IrisModel()
+#model = IrisModel()
+model = create_model(Configuration)
 
 # 3. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return the predicted flower species with the confidence
+""" Example:
 @app.post('/predict')
 def predict_species(iris: IrisSpecies):
     data = iris.dict()
@@ -19,8 +22,30 @@ def predict_species(iris: IrisSpecies):
         'prediction': prediction,
         'probability': probability
     }
+"""
 
+@app.post('/generate_images')
+def generate_images():
+    config = Configuration()
+    accelerator = Accelerator(
+        mixed_precision=config.mixed_precision,
+        gradient_accumulation_steps=config.gradient_accumulation_steps,
+        log_with="tensorboard",
+        project_dir=os.path.join(config.output_dir, "logs"),
+    )
 
+    image_generation_pipeline = initialize_pipeline(accelerator, config)
+
+    images = image_generation_pipeline(
+        batch_size=config.eval_batch_size,
+        generator=torch.manual_seed(config.seed),
+        num_inference_steps=config.number_of_noise_steps,
+    ).images  # type: ignore (There are multiple definitions which break the type hints)
+
+    if isinstance(images, list):
+        save_images(images)
+
+        
 # 4. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
