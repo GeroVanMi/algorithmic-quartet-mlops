5787951e1d62c12fb7fd6ec7f2ebcda484858680
Gerome Meyer <gerome.meyer@protonmail.com>
2024-05-15 14:48:48 +0200

    Training for longer duration

--- training/src/configurations/TrainConfig.py
+++ training/src/configurations/TrainConfig.py
@@ -7,5 +7,5 @@ from .Configuration import Configuration
 class TrainConfig(Configuration):
     train_batch_size = 96
     eval_batch_size = 8
-    num_epochs = 30
+    num_epochs = 60
     num_images = 7351
